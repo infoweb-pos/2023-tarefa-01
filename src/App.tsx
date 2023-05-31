485d7df5b3f5dd4aca436cc5b1cdfc2c8e58b130
João Pedro de França Barboza <franca.joao@academico.ifrn.edu.br>
2023-05-30 21:48:01 -0300

    Primeiro Commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+
+  const name = "João Pedro"
+  return (
+    <>
+    <h1> {name} </h1>
+
+    </>
+  )
+}
+
+export default App
